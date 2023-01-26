9800175a8a2ea69401bc3f8195edd35373bc53b1
Puker Who <nawwesele@gmail.com>
2023-01-27 01:03:10 +0200

    edit

--- footer.php
+++ footer.php
@@ -1,6 +1,21 @@
 </div>
 <!-- end wrap -->
 
+<footer>
+  <div class="container">
+    <div class="flex flex-col lg:flex-row lg:justify-between">
+      <div class="mb-4 lg:mb-0">
+        © Treba Solutions, 2023
+      </div>
+      <div>
+        <a href="https://webgolovolomki.com/">
+          <img src="https://treba-solutions.com/wp-content/uploads/2023/01/webg.jpg" alt="Web Головоломки" width="20">
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
